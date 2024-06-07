20b897267a875e95fb844a46d16c85b2789349c3
nicolas.dorier <nicolas.dorier@gmail.com>
2024-06-07 17:52:42 +0900

    Update list

--- Generators/Generate.ps1
+++ Generators/Generate.ps1
@@ -1,5 +1,5 @@
 dotnet build -c Release CancelMSM.Generators/CancelMSM.Generators.CLI/CancelMSM.Generators.CLI.csproj
 Function generate {
-  ./CancelMSM.Generators/CancelMSM.Generators.CLI/bin/Release/netcoreapp3.1/CancelMSM.Generators.CLI.exe $args
+  ./CancelMSM.Generators/CancelMSM.Generators.CLI/bin/Release/net8.0/CancelMSM.Generators.CLI.exe $args
  }
 generate --outputdir ".." selenium side -f "../Twitter.md"
